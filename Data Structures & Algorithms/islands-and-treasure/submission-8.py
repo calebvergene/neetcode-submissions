3c9237c7bab551cc235f52b8bd5a8c4a0af91bf5
Caleb Vergene <calebverg@gmail.com>
2026-09-13 13:35:26 -0700

    Add: islands-and-treasure - submission-8

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-8.py
@@ -0,0 +1,26 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        ROWS, COLS = len(grid), len(grid[0])
+        directions = [[0,1],[1,0],[-1,0],[0,-1]]
+
+        # first, get coords of treasure chests
+        q = deque()
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == 0:
+                    q.append([r,c])
+        
+        # now bfs from each chest 
+        distance = 0 
+        while q:
+            for _ in range(len(q)):
+                row, col = q.popleft()
+                if grid[row][col] == 2147483647:
+                    grid[row][col] = distance
+                for direction in directions:
+                    newr, newc = row + direction[0], col + direction[1]
+                    if newr in range(ROWS) and newc in range(COLS) and grid[newr][newc] == 2147483647:
+                        q.append([newr,newc])
+            
+            distance += 1
+                
