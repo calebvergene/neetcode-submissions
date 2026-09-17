b5263147369830512bbaae01fe84093b83d2498f
Caleb Vergene <calebverg@gmail.com>
2026-09-16 18:12:10 -0700

    Add: valid-tree - submission-1

---
+++ Data Structures & Algorithms/valid-tree/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+
+        adj = defaultdict(list)
+        for edge in edges:
+            adj[edge[0]].append(edge[1])        
+            adj[edge[1]].append(edge[0])
+
+        visited = set()
+
+        def check_cycle(node, path, prev):
+            nonlocal visited
+            if node in visited:
+                return False
+            if node in path:
+                return True
+            
+            path.add(node)
+            cycle = False
+            for neighbor in adj[node]:
+                # cannot dfs to prev node 
+                if neighbor != prev:
+                    cycle = cycle or check_cycle(neighbor, path, node)
+            path.remove(node)
+            visited.add(node)
+
+            return cycle
+        
+        for node in adj:
+            if check_cycle(node, set(), None):
+                return False
+        return True
+            
+
+                    
+
